f43b19ba6851679c034bd5b5153d271fe158ba7b
Nat Tessema <natnaeltessema1@gmail.com>
2025-04-10 10:58:14 -0700

    First update

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,45 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void ageCheck(int age) {
+        int LIMIT = 18;
+        boolean isAdult = age >= LIMIT; //comparison operator that generates a boolean value
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        if(isAdult) {
+            System.out.println("IS ADULT.");
+        }else{
+            System.out.println("IS *NOT* ADULT.");
         }
+    }
+
+    public static void main(String[] args) {
+        ageCheck(21);
+        ageCheck(12);
+        //COMPARISON OPERATORS - GENERATE A BOOLEAN VALUE (TRUE/FALSE)
+        System.out.println(25 == 21); //exactly equal
+        System.out.println(25 != 21); // not equal
+        System.out.println(25 > 21); // greater than
+        System.out.println(25 < 21); // less than
+        System.out.println(25 >= 21); // grater than or equal to
+        System.out.println(25 <= 21); // less than or equal
+
+
+        //LOGICAL OPERATORS && || !  - AND, OR, NOT
+
+        boolean isDiabetic = false;
+        boolean likesCake = true;
+
+        if(likesCake && !isDiabetic){ // if likes cake *and* is *not* diabetic
+            System.out.println("eats cake");
+        }
+
+        boolean isCat = true;
+        boolean isDog = false;
+        boolean isFriendly = false;
+
+        if((isCat || isDog) && isFriendly){ //if is a cat *or* is a dog then pet it.
+            System.out.println("Pet it.");
+        }else{
+            System.out.println("DON'T! pet it.");
+        }
+
     }
 }
